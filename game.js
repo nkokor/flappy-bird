cfc557161c4d1b368a59948ccf5bcf1068af1d15
Nikolina Kokor <nkokor@icloud.com>
2023-04-02 19:36:39 +0200

    createObstacle() implemented

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   let gameDiv = document.getElementById("game-div")
+  let sky = document.getElementById("sky-div")
   let ground = document.getElementById("ground-div")
   let bird = document.getElementById("bird-div")
 
@@ -9,17 +10,43 @@ document.addEventListener("DOMContentLoaded", () => {
   let gravity = 2
 
   function startGame() {
-    birdPositionBottom = birdPositionBottom - gravity
-    bird.style.left = birdPositionLeft + "px"
-    bird.style.bottom = birdPositionBottom + "px"
+    if(birdPositionBottom > 35) {
+      birdPositionBottom = birdPositionBottom - gravity
+      birdPositionBottom = birdPositionBottom - gravity
+      bird.style.left = birdPositionLeft + "px"
+      bird.style.bottom = birdPositionBottom + "px"
+    }
+    return
   }
 
   setInterval(startGame, 30)
 
-  function flyUp() {
-    birdPositionBottom = birdPositionBottom + 40
-    bird.style.bottom = birdPositionBottom
+
+  //flying
+  function flyUp(event) {
+    if(event.keyCode === 38 || event.keyCode === 32 || event.keyCode === 104) {
+      if(birdPositionBottom < 410) {
+        birdPositionBottom = birdPositionBottom + 40
+        bird.style.bottom = birdPositionBottom
+      }
+    }
   }
 
   document.addEventListener("keyup", flyUp)
+
+  //obstacles
+  function createObstacle() {
+    let obstaclePositionLeft = 550
+    let randomObstacleHeight = Math.random() * 270
+    let obstaclePositionBottom = 0
+    let obstacle = document.createElement('div')
+    obstacle.classList.add("obstacle-div")
+    sky.appendChild(obstacle)
+    obstacle.style.left = obstaclePositionLeft + "px"
+    obstacle.style.bottom = obstaclePositionBottom + "px"
+    console.log(randomObstacleHeight)
+    obstacle.style.height = randomObstacleHeight + "px"
+  }
+
+  createObstacle()
 })
